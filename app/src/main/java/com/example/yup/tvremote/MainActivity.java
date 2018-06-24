bb2b16acc4b5e7505cfc12ac17a7d223d2dbf857
TomStanley95 <tom.stanley@my.jcu.edu.au>
2018-06-24 21:51:35 +1000

    More work on XML + started work on sending TV remote (Samsung signals)

--- app/src/main/java/com/example/yup/tvremote/MainActivity.java
+++ app/src/main/java/com/example/yup/tvremote/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.yup.tvremote;
 
+import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.hardware.ConsumerIrManager.CarrierFrequencyRange;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    ConsumerIrManager remoteManager;
+    int tvFrequency = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        remoteManager = (ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE);
+
     }
 
 
@@ -20,45 +27,54 @@ public class MainActivity extends AppCompatActivity {
         final int id = v.getId();
         switch (id) {
             case R.id.volUp:
-                transmit(int carrierFrequency, int[] pattern);
+//                transmit( int carrierFrequency, int[] pattern);
                 System.out.println("Volume Up");
-
                 break;
-        }
-
-        switch (id) {
             case R.id.voldown:
                 System.out.println("Volume Down");
 
                 break;
-
-        }
-        switch (id) {
-            case R.id.up:
+            case R.id.channelUp:
                 System.out.println("Up");
                 break;
-        }
-
-        switch (id) {
-            case R.id.down:
+            case R.id.channelDown:
                 System.out.println("Down");
                 break;
-        }
-
-        switch (id) {
-            case R.id.scan:
-                System.out.println("Scan");
+            case R.id.setup:
+                remoteSetup();
+                System.out.println("Setup");
                 break;
-        }
-        switch (id) {
             case R.id.ok:
                 System.out.println("Ok");
                 break;
-        }
-        switch (id) {
             case R.id.power:
                 System.out.println("Power");
                 break;
+            case R.id.dpadUp:
+                System.out.println("Arrow Up");
+                break;
+            case R.id.dpadDown:
+                System.out.println("Arrow Down");
+                break;
+            case R.id.dpadLeft:
+                System.out.println("Arrow Left");
+                break;
+            case R.id.dpadRight:
+                System.out.println("Arrow Right");
+                break;
+        }
         }
 
-}}
+
+        public void remoteSetup(){
+        CarrierFrequencyRange[] frequencies = remoteManager.getCarrierFrequencies();
+        Log.i("Testing", Integer.toString(frequencies.length));
+        CarrierFrequencyRange frequency = frequencies[0];
+        Log.i("Testing", Integer.toString(frequency.getMaxFrequency()));
+        Log.i("Testing", Integer.toString(frequency.getMinFrequency()));
+
+        }
+
+
+}
+
